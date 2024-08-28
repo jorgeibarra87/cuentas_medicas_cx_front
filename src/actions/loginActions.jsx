aa11b5e69f79c7a4ddb23037a84f79857a2964a4
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-08-28 10:38:46 -0500

    Update

--- src/actions/loginActions.jsx
+++ src/actions/loginActions.jsx
@@ -1,8 +1,7 @@
-import { ALMACENAR_USERNAME, CERRAR_SESION, INICIAR_SESION, OBTENER_DECODE_TOKEN, OBTENER_TOKEN, OBTENER_USERNAME } from "../types";
+import { CERRAR_SESION, INICIAR_SESION, OBTENER_DECODE_TOKEN, OBTENER_TOKEN, OBTENER_USERNAME } from "../types";
 
 export const iniciarSesionAction = (data) => ({type: INICIAR_SESION, payload: data});
 export const obtenerToken = () => ({type: OBTENER_TOKEN});
 export const obtenerDecodeToken = () => ({type: OBTENER_DECODE_TOKEN});
 export const cerrarSesionAction = () => ({type: CERRAR_SESION});
-export const almacenarUsername = (data) => ({type: ALMACENAR_USERNAME, payload: data});
 export const obtenerUsername = () => ({type: OBTENER_USERNAME});
