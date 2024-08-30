1717ea1bea6e6fe961a07b2a59e2b1053ceccce6
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-08-30 13:40:30 -0500

    quitando warning

--- src/components/nav/Sidebar.jsx
+++ src/components/nav/Sidebar.jsx
@@ -3,7 +3,7 @@ import imgLogo from '../../img/favicon.ico'
 import Navbar from './Navbar'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 export default function Sidebar({ componente: Componente }) {
 
     const stateSidebar = useSelector(state => state.sidebar);
@@ -126,6 +126,6 @@ export default function Sidebar({ componente: Componente }) {
 }
 
 //agregamos para que se valinden errores de prop.types
-Sidebar.propTypes = {
-    componente: PropTypes.elementType.isRequired,
-}
+// Sidebar.propTypes = {
+//     componente: PropTypes.elementType.isRequired,
+// }
