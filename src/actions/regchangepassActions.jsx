8f425acbab1d7dec4e9560e4d59c05bcab16c02e
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-11-29 14:18:34 -0500

    remove registro change

--- src/actions/regchangepassActions.jsx
+++
@@ -1,4 +0,0 @@
-import { REGCHANGEPASS_INFO_USUARIO, REGCHANGEPASS_TIME_EXPIRED } from "../types";
-
-export const regchangepassInfoUser = (data) => ({type: REGCHANGEPASS_INFO_USUARIO, payload: data});
-export const regchangepassTimeExpired = (data) => ({type: REGCHANGEPASS_TIME_EXPIRED, payload: data});
