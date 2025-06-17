747dbba83f405e38185ed760dc0819c68c4a1587
desahusjp <lcalvarez@unicauca.edu.co>
2025-06-17 17:49:54 -0500

    Update apiClienteSistemas.jsx

--- src/api/sistemas/apiClienteSistemas.jsx
+++ src/api/sistemas/apiClienteSistemas.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClienteSistemas = axios.create({
-    baseURL: `http://optimus:8100/sistemas-microservice/`,
+    baseURL: `http://optimus:8100/microservice-sistemas/`,
 });
 
 apiClienteSistemas.interceptors.request.use((config) => {
