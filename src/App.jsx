9cc50562a1e827d41d2bb5f0aca984271c0a707e
desahusjp <lcalvarez@unicauca.edu.co>
2025-06-20 17:07:41 -0500

    prueba archivo externo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 import store from './store'
 import RutasConfig from './components/config/RutasConfig'
 
+const apiUrl = window.env.VITE_URL_API_GATEWAY;
+
 function App() {
+  console.log("API URL:", apiUrl);
   return (
     <>
       <Provider store={store}>
