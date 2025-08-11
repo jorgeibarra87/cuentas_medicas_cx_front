0475c5e7060621280da90547bfc9bd4d8cf1bb13
desahusjp <lcalvarez@unicauca.edu.co>
2025-08-11 15:36:20 -0500

    Update adnIngresoService.jsx

--- src/api/dinamicados/adnIngresoService.jsx
+++ src/api/dinamicados/adnIngresoService.jsx
@@ -3,7 +3,7 @@ import apiClienteDinamica from './apiClienteDinamicados';
 
 export const obtenerAdnIngreso = async (idAdnIngreso) => {
     try {
-        const response = await apiClienteDinamica.get(`/dinamica/api/adnIngreso/GenPacien/${idAdnIngreso}`);
+        const response = await apiClienteDinamica.get(`adnIngreso/GenPacien/${idAdnIngreso}`);
         return new AdnIngreso(response.data);
     }catch (error) {
         console.error('Error al obtener el adnIngreso',error);
