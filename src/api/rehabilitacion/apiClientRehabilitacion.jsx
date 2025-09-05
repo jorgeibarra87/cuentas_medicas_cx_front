2db6d1261241669c6927f0d1bb205e406b785154
desahusjp <lcalvarez@unicauca.edu.co>
2025-09-05 17:40:53 -0500

    Update apiClientRehabilitacion.jsx

--- src/api/rehabilitacion/apiClientRehabilitacion.jsx
+++ src/api/rehabilitacion/apiClientRehabilitacion.jsx
@@ -1,10 +1,9 @@
 import axios from "axios";
+import { createApiClient } from "../apiClientFactory";
 
 const ruta = window.env.VITE_URL_API_GATEWAY;
 const rutaMicroservicioRehabilitacion = window.env.VITE_URL_REHABILITACION;
 
-const apiClientRehabilitacion = axios.create({
-  baseURL: ruta + rutaMicroservicioRehabilitacion,
-});
+const apiClientRehabilitacion = createApiClient(`${ruta}${rutaMicroservicioRehabilitacion}`);
 
 export default apiClientRehabilitacion;
