eceebb503f96c7ae79aff8350f9dea542c616192
jalvarez <61236470+snkalvarez@users.noreply.github.com>
2026-03-17 14:07:15 -0500

    feat: monitorizacion rehabilitacion
    separamos preguntas de medico, para ponerlas en rehabilitación

--- src/components/nav/Sidebar.jsx
+++ src/components/nav/Sidebar.jsx
@@ -103,6 +103,7 @@ export default function Sidebar({ componente: Componente }) {
                 {nombre: 'Monitorizacion Medico', ruta: '/monitorizacionhc/preguntas/medico', roles: ['ROLE_ADMINISTRADOR','ROLE_MONITORIZACION_MEDICO']},
                 {nombre: 'Monitorizacion Enfermeria', ruta: '/monitorizacionhc/preguntas/enfermeria', roles: ['ROLE_ADMINISTRADOR','ROLE_MONITORIZACION_ENFERMERIA']},
                 {nombre: 'Monitorización Facturacion', ruta: '/monitorizacionhc/preguntas/facturacion', roles: ['ROLE_ADMINISTRADOR','ROLE_MONITORIZACION_FACTURACION']},
+                {nombre: 'Monitorización Rehabilitación', ruta: '/monitorizacionhc/preguntas/rehabilitacion', roles: ['ROLE_ADMINISTRADOR','ROLE_MONITORIZACION_REHABILITACION']},
                 {nombre: 'Reportes', ruta: '/monitorizacionhc/reportes', roles: ['ROLE_ADMINISTRADOR','ROLE_MONITORIZACION']},
                 {nombre: 'Ajustes', ruta: '/monitorizacionhc/ajustes', roles: ['ROLE_ADMINISTRADOR']}
             ]
