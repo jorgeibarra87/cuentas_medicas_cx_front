6b1d0c8ef0c67491061289f1a2fd5a9477449ae1
Jorge Armando Ibarra Palacios <jorgeibarra87@gmail.com>
2026-07-17 09:14:12 -0500

    ajuste de rutas apicleintfactory 1

--- src/modules/referencia-contrareferencia/api/apiClienteReferenciaContrareferencia.jsx
+++ src/modules/referencia-contrareferencia/api/apiClienteReferenciaContrareferencia.jsx
@@ -1,7 +1,10 @@
 import { createApiClient } from "../../../shared/api/apiClientFactory";
 
-const ruta = (window.API_URL && !window.API_URL.startsWith("__")) ? window.API_URL : "http://localhost:8100";;
+//const ruta = (window.API_URL && !window.API_URL.startsWith("__")) ? window.API_URL : "http://localhost:8100";;
+//const apiClientReferenciaContrareferencia = createApiClient(`${ruta}/api-referencia/`);
+const ruta = window.env.VITE_URL_API_GATEWAY;
+const micro = window.env.VITE_URL_REFERENCIA_CONTRAREFERENCIA;
 
-const apiClientReferenciaContrareferencia = createApiClient(`${ruta}/api-referencia/`);
+const apiClientReferenciaContrareferencia = createApiClient(`${ruta}${micro}/`);
 
 export default apiClientReferenciaContrareferencia;
