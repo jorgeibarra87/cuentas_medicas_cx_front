df539f40d9df657c999de02f1ea24edf2b70882a
Jorge Armando Ibarra Palacios <jorgeibarra87@gmail.com>
2026-07-28 09:23:08 -0500

    feat(egreso) se agrega hook para actualisacion periodica de fecha egreso

--- src/shared/components/Sidebar.jsx
+++ src/shared/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { appMenu } from '../menu';
 import { filtrarMenu } from '../menu/filterMenu';
 import SidebarItem from './SidebarItem';
+import useActualizarEgresoPeriodico from '../../modules/anexo1/hooks/useActualizarEgresoPeriodico';
 
 export default function Sidebar({ componente: Componente }) {
 
@@ -13,6 +14,8 @@ export default function Sidebar({ componente: Componente }) {
   const statelogin = useSelector(state => state.login);
   const usuario = statelogin.decodeToken;
   const [abierto, setAbierto] = useState(null);
+
+  useActualizarEgresoPeriodico(!!usuario);
 
   const [submenuAbierto, setSubmenuAbierto] = useState(null);
   const location = useLocation();
