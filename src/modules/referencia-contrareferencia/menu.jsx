9a48756e574ccf4fe11c12fcc9835e43cbc084b4
Jorge Armando Ibarra Palacios <jorgeibarra87@gmail.com>
2026-07-29 12:01:02 -0500

    revert(menu index) se revierten cambios de menu index y se mueven menus de admin a modulo de referencia

--- src/modules/referencia-contrareferencia/menu.jsx
+++ src/modules/referencia-contrareferencia/menu.jsx
@@ -26,6 +26,15 @@ export const referenciaContrareferenciaMenu = {
       nombre: 'Anexo 1',
       ruta: '/anexo1/general',
       roles: ['ROLE_ADMINISTRADOR', 'ROLE_REFERENCIA_ANEXO1']
-    }
+    },
+    { nombre: 'Admin Traslados', ruta: '/admin/traslados', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Cuentas Médicas', ruta: '/admin/cuentas-medicas', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Facturaciones', ruta: '/admin/facturaciones', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Pacientes', ruta: '/admin/pacientes', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Trámites', ruta: '/admin/tramites', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Seg. Intra', ruta: '/admin/seguimiento-intra', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Seg. Amb.', ruta: '/admin/seguimiento-ambulatorio', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Egresos', ruta: '/admin/egresos', roles: ['ROLE_ADMINISTRADOR'] },
+    { nombre: 'Admin Tipos Solicitud', ruta: '/admin/tipos-solicitud', roles: ['ROLE_ADMINISTRADOR'] }
   ]
 };
