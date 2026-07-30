58ae0db21d9617218bee7a896b989a36080c9a68
Jorge Armando Ibarra Palacios <jorgeibarra87@gmail.com>
2026-07-30 16:19:29 -0500

    feat(anexo1) ajuste de archivos para merge de anexo1(tramites) y admin de traslado y tramite 2

--- src/modules/auth/api/apiClientAuthServicePublic.jsx
+++ src/modules/auth/api/apiClientAuthServicePublic.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 // IMPORTAR CLAVES ENV DE VITE 
-const ruta = (window.API_URL && !window.API_URL.startsWith("__")) ? window.API_URL : "http://localhost:8100";
+ const ruta = (window.API_URL && !window.API_URL.startsWith("__")) ? window.API_URL : "http://localhost:8100";
 
-const apiClientAuthServicePublic = axios.create({
+ const apiClientAuthServicePublic = axios.create({
     baseURL: `${ruta}/api-auth/`,
     headers: {
         'X-Public-Route': 'true',
     },
-});
+ });
 
-export default apiClientAuthServicePublic;
+ export default apiClientAuthServicePublic;
